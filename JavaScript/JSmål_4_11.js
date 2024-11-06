31b78433b31ea7478b65d071f3b5fa07bcb49fd4
veggenss <veggens.v@gmail.com>
2024-11-06 13:05:25 +0100

    JS

--- JavaScript/JSmål_4_11.js
+++ JavaScript/JSmål_4_11.js
@@ -1,1 +1,11 @@
 /*Java Script med bent*/
+
+document.getElementById("addTaskButton").addEventListener("click", addTask)
+
+function addTask(){
+    let task = document.getElementById("taskInput").value;
+    let listenItem = document.createElement("li");
+    listenItem.textContent = task
+
+    document.getElementById("taskList").appendChild(listItem)
+}

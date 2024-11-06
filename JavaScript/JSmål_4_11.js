99aa09c040470d2b22dd718f9323fc2ea1c254b3
veggenss <veggens.v@gmail.com>
2024-11-06 13:06:21 +0100

    hah

--- JavaScript/JSmål_4_11.js
+++ JavaScript/JSmål_4_11.js
@@ -1,11 +1,27 @@
 /*Java Script med bent*/
 
-document.getElementById("addTaskButton").addEventListener("click", addTask)
+document.getElementById("addTaskButton").addEventListener("click", addTask);
+document.getElementById("removeTaskButton").addEventListener("click", removeTask);
 
 function addTask(){
     let task = document.getElementById("taskInput").value;
-    let listenItem = document.createElement("li");
-    listenItem.textContent = task
+    let listItem = document.createElement("li");
+    listItem.textContent = task;
 
-    document.getElementById("taskList").appendChild(listItem)
+    document.getElementById("taskList").appendChild(listItem);
+    document.getElementById("taskInput").value = "";
+}
+
+function removeTask(){
+
+    let task = document.getElementById("taskInput").value;
+    let taskList = document.getElementById("taskList");
+    let listItems = taskList.getElementsByTagName("li");
+
+    for (let i = 0; i < listItems.length; i++){
+        if (listItems[i].textContent === task){
+            taskList.removeChild(listItems[i]);
+            return
+        }
+    }
 }

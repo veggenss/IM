0c3ca8ea0f9646aff4b1e1303f21dfa17fb7e3a2
veggenss <veggens.v@gmail.com>
2024-12-11 15:38:50 +0100

    dasd

--- Min Side/JS/log.js
+++ Min Side/JS/log.js
@@ -1,19 +1,35 @@
-//Logg underside - Søke funksjon
+//Checks if there is an input in the search bar
 document.getElementById('search').addEventListener('input', filterLogs);
 
-// Listen for ENTER key press for responsive search
-document.getElementById('search').addEventListener('keydown', (event) => {
-    if (event.key === "Enter") {
-        filterLogs();
-    }
-});
+// Add event listener for month filter
+document.getElementById('filterSelect').addEventListener('change', filterByMonth);
+
+function filterByMonth() {
+    const selectedMonth = document.getElementById('filterSelect').value.toLowerCase();
+    const logs = document.querySelectorAll('.log');
+
+    logs.forEach(log => {
+        if (selectedMonth === 'all') {
+            log.style.display = 'block';
+        } 
+        else {
+            const monthId = log.id.toLowerCase();
+            
+            if (monthId.includes(selectedMonth)) {
+                log.style.display = 'block';
+            } else {
+                log.style.display = 'none';
+            }
+        }
+    });
+}
 
 function filterLogs() {
     const searchInput = document.getElementById('search').value.toLowerCase();
     const logs = document.querySelectorAll('.log');
 
     logs.forEach(log => {
-        const titleElement = log.querySelector('h3');
+        const titleElement = log.querySelector('h3', "h6");
         const title = titleElement.textContent.toLowerCase();
 
         // Check if search input matches either title or content

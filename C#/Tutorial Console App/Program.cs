95e82e80b47ed47fb94797490f1b4678c0f32f22
veggenss <veggens.v@gmail.com>
2025-04-09 13:55:50 +0200

    C# Prosjekter

---
+++ C#/Tutorial Console App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tutorial_Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0} {1}", long.MinValue, long.MaxValue);
+        }
+    }
+}
